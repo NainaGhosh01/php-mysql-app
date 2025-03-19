4b85cc7b3188b028244ca6e0b37ea5cd0495c89f
NainaGhosh01 <nainaghosh01@gmail.com>
2025-03-19 19:48:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'secure-database-1.cluster-ch0884o2o8rn.us-east-1.rds.amazonaws.com');
 define('DB_USER', 'admin');
 define('DB_PASS', 'Ng21-01-2003');
-define('DB_NAME', 'secure-database-1');
+define('DB_NAME', 'myappdb');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
